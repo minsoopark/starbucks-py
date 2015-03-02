2d0f4815a3f566eefb1327184ef1724c93f4cd3a
Minsoo Park <minsoo1003@gmail.com>
2015-03-03 00:10:35 +0900

    Fix contents of setup.py

--- setup.py
+++ setup.py
@@ -4,12 +4,11 @@ setup(
     name='Starbucks',
     packages=['starbucks'],
     version='0.0.1',
-    description='An unoffical API for Starbucks.',
+    description='Unoffical Starbucks API.',
     long_description=open('README.rst').read(),
     license='BSD License',
     author='Minsoo Park',
     author_email='minsoo1003@gmail.com',
     url='https://github.com/minsoopark/starbucks-py',
     keywords=['Starbucks'],
-    classifiers=[],
 )
